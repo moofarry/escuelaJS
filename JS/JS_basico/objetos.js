870f16c4dd5a9b917312db6d61f5713057a1cd57
moofarry <jhon.moofarry00@usc.edu.co>
2020-11-09 10:21:02 -0500

    update

--- JS/JS_basico/objetos.js
+++
@@ -1,9 +0,0 @@
-var miAuto = {
-    marca : "Toyota",
-    modelo : "Corolla",
-    annio: 2020,
-    detalleAuto: function() {
-        console.log(`Auto ${this.modelo} ${this.annio}`)
-        
-    }
-};
